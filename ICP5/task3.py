71822877165690fb52f4830b73fbebac0d9b661d
Adavellisahaja <54335540+Adavellisahaja@users.noreply.github.com>
2020-04-24 22:43:13 -0500

    commit

--- ICP5/task3.py
+++ ICP5/task3.py
@@ -1,5 +1,5 @@
-import numpy as np
-import pandas as pd
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 from sklearn.feature_extraction.text import CountVectorizer
 from keras.preprocessing.text import Tokenizer
@@ -12,65 +12,36 @@ import re
 
 from sklearn.preprocessing import LabelEncoder
 
-data = pd.read_csv('spam.csv')
+data = pd.read_csv('spam.csv', encoding='latin-1')
 # Keeping only the neccessary columns
-data = data[['text','sentiment']]
-data = data[data.sentiment != 'Neutral']
-
-data = data[data.sentiment != "Neutral"]
-data['text'] = data['text'].apply(lambda x: x.lower())
-data['text'] = data['text'].apply((lambda x: re.sub('[^a-zA-z0-9\s]', '', x)))
-
-print(data[data['sentiment'] == 'Positive'].size)
-print(data[data['sentiment'] == 'Negative'].size)
-
+data = data[['v1','v2']]
+data['v2'] = data['v2'].apply(lambda x: x.lower())
+data['v2'] = data['v2'].apply((lambda x: re.sub('[^a-zA-z0-9\s]', '', x)))
 for idx, row in data.iterrows():
     row[0] = row[0].replace('rt', ' ')
-
 max_fatures = 2000
 tokenizer = Tokenizer(num_words=max_fatures, split=' ')
-tokenizer.fit_on_texts(data['text'].values)
-X = tokenizer.texts_to_sequences(data['text'].values)
+tokenizer.fit_on_texts(data['v1'].values)
+X = tokenizer.texts_to_sequences(data['v1'].values)
 print(X)
 X = pad_sequences(X)
-print(X)
-embed_dim =7
-lstm_out = 196
-def createmodel():
-    model = Sequential()
-    model.add(Embedding(max_fatures, embed_dim,input_length = X.shape[1]))
-    model.add(SpatialDropout1D(0.4))
-    model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
-    model.add(Dense(2,activation='softmax'))
-    model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
-    return model
-# print(model.summary())
-
 labelencoder = LabelEncoder()
-integer_encoded = labelencoder.fit_transform(data['sentiment'])
+integer_encoded = labelencoder.fit_transform(data['v1'])
 y = to_categorical(integer_encoded)
 X_train, X_test, Y_train, Y_test = train_test_split(X,y, test_size = 0.33, random_state = 42)
-print(X_train.shape,Y_train.shape)
-print(X_test.shape,Y_test.shape)
-
+def createmodel1():
+  embed_dim=128
+  lstm_out=196
+  model = Sequential()
+  model.add(Embedding(max_fatures, embed_dim,input_length = X.shape[1]))
+  model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
+  model.add(Dense(2,activation='softmax'))
+  model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
+  return model
 batch_size = 32
-model = createmodel()
-model.fit(X_train, Y_train, epochs = 5, batch_size=batch_size, verbose = 2)
-from keras.models import load_model
-
-model.save('my_model.h5')
-model = load_model('my_model.h5')
-import keras.preprocessing.text
-
-text = np.array(['this is just some random, stupid text'])
-print(text.shape)
-pred = tokenizer.texts_to_sequences(text)
-pred=pad_sequences(pred,maxlen=28)
-print(pred)
-
-prediction = model.predict(pred)
-print(pred)
-##
-score,acc = model.evaluate(X_test,Y_test,verbose=2,batch_size=batch_size)
+model1 = createmodel1()
+model1.fit(X_train, Y_train, epochs = 5, batch_size=batch_size, verbose = 2)
+score,acc = model1.evaluate(X_test,Y_test,verbose=2,batch_size=batch_size)
 print(score)
 print(acc)
+print(model1.metrics_names)
